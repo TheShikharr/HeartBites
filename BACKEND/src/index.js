130f1795e5421c69389159a1a2f69445c34c9c9f
ShikharSunyal <shikhar2950@gmail.com>
2026-03-31 00:06:37 +0530

    Signup Auth

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import {connectdb} from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js"
 
@@ -7,6 +8,13 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
+
+// MIDDLEWARES
+app.use(express.json())
+
 app.use('/api/auth', authRoutes)
 
-app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`))
+app.listen(PORT, () => {
+    console.log(`Server Started at PORT: ${PORT}`)
+    connectdb()
+})
