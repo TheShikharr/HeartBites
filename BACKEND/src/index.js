e0a19f5b66fff8321e18cfd136ead72f72c4d635
ShikharSunyal <shikhar2950@gmail.com>
2026-04-02 21:13:25 +0530

    messages added

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+
 import {connectdb} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
 import matchRoutes from "./routes/match.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 
 dotenv.config()
 const app = express()
@@ -20,8 +23,13 @@ app.use(cors({
     credentials: true
 }))
 
+
+// ROUTES
 app.use('/api/auth', authRoutes)
 app.use('/api/match', matchRoutes)
+app.use('/api/messages', messageRoutes)
+
+
 
 app.listen(PORT, () => {
     console.log(`Server Started at PORT: ${PORT}`)
