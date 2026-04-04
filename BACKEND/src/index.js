4a4ee37dd45763e8ca5a85c6adfa5564af62b32b
ShikharSunyal <shikhar2950@gmail.com>
2026-04-05 01:36:37 +0530

    cloudinary updated

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT
 
 
 // MIDDLEWARES
-app.use(express.json())
+
+app.use(express.json({ limit: "10mb" }))
+app.use(express.urlencoded({ limit: "10mb", extended: true }))
+
 app.use(cookieParser())
 app.use(cors({
     origin: "http://localhost:5173",
