b9a3cd1ab739fd5f0a770893cee71eae6ce83c34
ShikharSunyal <shikhar2950@gmail.com>
2026-04-06 17:52:32 +0530

    nodemailer updated

--- BACKEND/src/lib/nodemailer.js
+++ BACKEND/src/lib/nodemailer.js
@@ -4,16 +4,16 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,              // ✅ use 587 instead of 465
-    secure: false,          // ✅ false for port 587
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     }
 })
 
-export const sendEmailOTP = async (email, otp) => {
+export const sendOTPEmail = async (email, otp) => {
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: email,
@@ -21,7 +21,9 @@ export const sendEmailOTP = async (email, otp) => {
         html: `
             <h2>Welcome to HeartBites! </h2>
             <p>Your OTP for email verification is:</p>
-            <h1 style="color: #7c3aed; font-size: 40px; letter-spacing: 5px">${otp}</h1>
+            <h1 style="color: #7c3aed; font-size: 40px; letter-spacing: 5px">
+                ${otp}
+            </h1>
             <p>This OTP will expire in <b>10 minutes</b>.</p>
             <p>If you did not create an account, please ignore this email.</p>
         `
